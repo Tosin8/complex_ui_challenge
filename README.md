1b3a431c669224f5882e6de94b8c8dea85bfb9f0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-26 22:38:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# complex_ui_challenge
+
+Playlist of complex ui challenge projects.
+
+The triangle of complex ui are: 
+1. Stack 
+2. Transform 
+3. Animation Controller
+
+## Getting Started
+
---
+++ lib/marcin_slazek/complex_1.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    var myDrawer = Container(color: Colors.blue);
+    var myContent = Container(color: Colors.yellow);
+    return Stack(children: [
+      myDrawer,
+      // Transform.scale(scale: 0.5, child: myContent), -used the primary,basic transform scale.
+
+      // Using Matrix transform for explict.
+      Transform(
+        transform: Matrix4.identity()..scale(0.5),
+        alignment: Alignment.centerLeft,
+        child: myContent,
+      ),
+    ]);
+  }
+}
