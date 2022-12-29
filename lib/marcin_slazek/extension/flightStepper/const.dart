7b410f96580b798c92096743434539c8b4110aba
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-29 23:24:46 +0100

    Updating with plan state class

--- lib/marcin_slazek/extension/flightStepper/const.dart
+++ lib/marcin_slazek/extension/flightStepper/const.dart
@@ -26,10 +26,39 @@ class Plane extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const Positioned(
+        left: 40,
+        top: 32,
+        child: RotatedBox(
+          quarterTurns: 2,
+          child: Icon(
+            Icons.airplanemode_active,
+            size: 64,
+          ),
+        ));
+  }
+}
+
+class Line extends StatelessWidget {
+  const Line({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Positioned(
-      left: 40, 
-      top: 32, 
-      child: 
+      left: 40.0 + 32,
+      top: 40,
+      bottom: 0,
+      width: 1,
+      child: Container(color: Colors.white.withOpacity(0.5)),
     );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
