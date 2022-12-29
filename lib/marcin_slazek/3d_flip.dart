5b937d45f83b5f87d09d43f1b55a26374279d249
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-29 21:05:56 +0100

    Update 3d_flip.dart

--- lib/marcin_slazek/3d_flip.dart
+++ lib/marcin_slazek/3d_flip.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Flip extends StatefulWidget {
+  const Flip({super.key});
+
+  @override
+  State<Flip> createState() => _FlipState();
+}
+
+class _FlipState extends State<Flip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
