c409924a32e5a9d9c57d8b1240428a7a3d842580
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-12-29 23:02:48 +0100

    Updating with Stack children

--- lib/marcin_slazek/flight_stepper.dart
+++ lib/marcin_slazek/flight_stepper.dart
@@ -11,20 +11,21 @@ class _FlightStepperState extends State<FlightStepper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: 
-        Container(
-  decoration: const BoxDecoration(
-    gradient: LinearGradient(
-      begin: Alignment.centerLeft,
-      end: Alignment.centerRight,
-      colors: [Colors.purple, Color(0xFF4A148C)])
-  ),
-)
-       
+        body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.bottomCenter,
+              colors: [Colors.purple, Color(0xFF4A148C)])),
       child: SafeArea(
           child: Stack(
-        children: const [],
+        children: const [
+          ArrowIcons(),
+          Plane(),
+          Line(),
+          Page(),
+        ],
       )),
-    ); 
+    ));
   }
 }
